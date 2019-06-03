c638ebfe11d6860dff3d541eb02c710707523a31
maxdavid <me@maxdavid.com>
2019-06-03 14:27:11 -0700

    Add base components boilerplate
    + SearchBar
    + PostContainer
    + CommentSection

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
+
+export default CommentSection;
