13029277104f64645ac045240bfea0a6594661bf
maxdavid <me@maxdavid.com>
2019-06-03 15:25:16 -0700

    Add dummy-data to assets

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import dummyData from './assets/dummy-data';
+
 function App() {
   return (
     <div className="App">
