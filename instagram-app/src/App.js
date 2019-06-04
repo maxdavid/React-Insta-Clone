e1e9e815a34d0d467aba09a970db7210b5398996
maxdavid <me@maxdavid.com>
2019-06-03 17:41:22 -0700

    Add styles and logic for PostContainer

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.scss';
 
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 import dummyData from './assets/dummy-data';
 
 function App() {
   return (
-    <SearchBar />
+    <section>
+      <SearchBar />
+      <main role='main'>
+        <section className='social-feed'>
+          {dummyData.map((post) => (
+            <PostContainer data={post} />
+          ))}
+        </section>
+      </main>
+    </section>
   );
 }
 
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import './PostContainer.scss';
+
+import CommentSection from '../CommentSection/CommentSection';
 
 class PostContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = props.data;
   }
 
   render() {
+    return (
+      <article className='post-container'>
+        <header>
+          <div className='poster-avatar'>
+            <a>
+              <img src={this.state.thumbnailUrl} alt="{this.state.username}'s profile picture" />
+            </a>
+          </div>
+          <div className='poster-handle'>
+            <h2>
+              <a href='' title={this.state.username}>
+                {this.state.username}
+              </a>
+            </h2>
+          </div>
+        </header>
+        <div className='post-image'>
+          <img src={this.state.imageUrl} alt="{this.state.username}'s image post" />
+        </div>
+        
+        <div className='interaction-buttons'>
+          <button>
+            <span className='heart-glyph' />
+          </button>
+        </div>
 
+      <CommentSection comments={this.state.comments} />
+
+      </article>
+    )
   }
 }
 
